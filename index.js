f0a7adb0d29890495da2d12f092ab1f9d477f289
Zainab Saud <zai99nb@gmail.com>
2020-06-16 09:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Formatter {
   static titleize(string){
     let  words = ["the", "a", "an", "but", "of", "and", "for", "at", "by","from"]
     let result = string.split(' ')
-    for(let i = 0 ; i < string.length;i++){
+    for(let i = 0 ; i < result.length;i++){
      result[i]= result[i].charAt(0).toUpperCase() + result[i].slice(1)
     }
     return result.join(' ')
