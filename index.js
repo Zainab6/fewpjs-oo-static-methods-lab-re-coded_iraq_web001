b73e320d926814b2afa45c189cecfaa4014e4ee6
Zainab Saud <zai99nb@gmail.com>
2020-06-16 09:22:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Formatter {
     let result = string.split(' ')
     for(let i = 0 ; i < result.length;i++){
      result[i]= result[i].charAt(0).toUpperCase() + result[i].slice(1)
-     if (words.includes(result[i])){
+     if (words.includes( result[i] ) ){
        return words
      }
     }
